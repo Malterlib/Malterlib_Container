2c6900bb7dd9ea6624801a1353f9a958526a9253
Erik Olofsson <erik.olofsson@hansoft.com>
2016-01-25 14:22:53 +0000

    [Integrate] //Malterlib/Main -> //Malterlib/Dev
    [Maltelib/Concurrency]
    Add support for TCActor->f_Destroy with current actor deduced (integrated)
    
    [Malterlib/Concurrency]
    Add checks for forgetting to finish actor calls (integrated)
    Add helper TCContinuation::fs_RunProtected to make it easier to forward exception to the TCContinuation result (integrated)
    
    [Malterlib/Container]
    Fix bug where first parameter of fg_CreateVector would be lost (integrated)
    
    [Malterlib/Core]
    Add missing MTool path (integrated)
    
    [Malterlib/Encoding]
    Fix initialization error (integrated)
    
    [Malterlib/Time]
    Add more formatting options in CTime (integrated)
    
    Copied from Perforce
     Change: 56997

--- Source/Malterlib_Container_Vector.h
+++ Source/Malterlib_Container_Vector.h
@@ -2555,7 +2555,7 @@ namespace NMib
 		TCVector<typename NTraits::TCRemoveReference<tf_CFirst>::CType> fg_CreateVector(tf_CFirst && _First, tf_CParams && ..._Params)
 		{
 			TCVector<typename NTraits::TCRemoveReference<tf_CFirst>::CType> Return;
-			fg_CreateVectorHelper<typename NTraits::TCRemoveReference<tf_CFirst>::CType>(Return, fg_Forward<tf_CParams>(_Params)...);
+			fg_CreateVectorHelper<typename NTraits::TCRemoveReference<tf_CFirst>::CType>(Return, fg_Forward<tf_CFirst>(_First), fg_Forward<tf_CParams>(_Params)...);
 			return Return;
 		}
 
